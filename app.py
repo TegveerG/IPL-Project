4d7fa41161ca157c794b311dcc7c614f9a8bb985
Tegveer Ghura <tegveerg@bu.edu>
2023-10-30 20:18:33 -0400

    app functioning well, added runs_left feature, use new XGBoost pkl file for deployment

--- app.py
+++ app.py
@@ -4,61 +4,31 @@ import pickle
 import numpy as np
 
 # Load the Random Forest CLassifier model
-filename = "pickle_files/XGB.pkl"
+filename = "./products/pickle_files/XGB_deploy.pkl"
 classifier = pickle.load(open(filename, "rb"))
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
     return render_template("index.html")
-
 
 @app.route("/predict", methods=["POST"])
 def predict():
     temp_array = list()
 
     if request.method == "POST":
-        batting_team = request.form["batting-team"]
-        if batting_team == "Chennai Super Kings":
-            temp_array = temp_array + [1, 0, 0, 0, 0, 0, 0, 0]
-        elif batting_team == "Delhi Daredevils":
-            temp_array = temp_array + [0, 1, 0, 0, 0, 0, 0, 0]
-        elif batting_team == "Kings XI Punjab":
-            temp_array = temp_array + [0, 0, 1, 0, 0, 0, 0, 0]
-        elif batting_team == "Kolkata Knight Riders":
-            temp_array = temp_array + [0, 0, 0, 1, 0, 0, 0, 0]
-        elif batting_team == "Mumbai Indians":
-            temp_array = temp_array + [0, 0, 0, 0, 1, 0, 0, 0]
-        elif batting_team == "Rajasthan Royals":
-            temp_array = temp_array + [0, 0, 0, 0, 0, 1, 0, 0]
-        elif batting_team == "Royal Challengers Bangalore":
-            temp_array = temp_array + [0, 0, 0, 0, 0, 0, 1, 0]
-        elif batting_team == "Sunrisers Hyderabad":
-            temp_array = temp_array + [0, 0, 0, 0, 0, 0, 0, 1]
-
-        bowling_team = request.form["bowling-team"]
-        if bowling_team == "Chennai Super Kings":
-            temp_array = temp_array + [1, 0, 0, 0, 0, 0, 0, 0]
-        elif bowling_team == "Delhi Daredevils":
-            temp_array = temp_array + [0, 1, 0, 0, 0, 0, 0, 0]
-        elif bowling_team == "Kings XI Punjab":
-            temp_array = temp_array + [0, 0, 1, 0, 0, 0, 0, 0]
-        elif bowling_team == "Kolkata Knight Riders":
-            temp_array = temp_array + [0, 0, 0, 1, 0, 0, 0, 0]
-        elif bowling_team == "Mumbai Indians":
-            temp_array = temp_array + [0, 0, 0, 0, 1, 0, 0, 0]
-        elif bowling_team == "Rajasthan Royals":
-            temp_array = temp_array + [0, 0, 0, 0, 0, 1, 0, 0]
-        elif bowling_team == "Royal Challengers Bangalore":
-            temp_array = temp_array + [0, 0, 0, 0, 0, 0, 1, 0]
-        elif bowling_team == "Sunrisers Hyderabad":
-            temp_array = temp_array + [0, 0, 0, 0, 0, 0, 0, 1]
-
         overs = float(request.form["Overs"])
+        temp_array = temp_array + [overs]
+
         wickets = int(request.form["Wickets-Fallen"])
+        temp_array = temp_array + [wickets]
+
         runs = int(request.form["Runs-Scored"])
+        temp_array = temp_array + [runs]
+
+        runs_left = int(request.form["Runs-Left"])
+        temp_array = temp_array + [runs_left]
 
         match_type = request.form["Match-Type"]
         if match_type == "playoffs":
@@ -108,6 +78,42 @@ def predict():
         elif inning == 2:
             temp_array = temp_array + [0, 1]
 
+        batting_team = request.form["batting-team"]
+        if batting_team == "Chennai Super Kings":
+            temp_array = temp_array + [1, 0, 0, 0, 0, 0, 0, 0]
+        elif batting_team == "Delhi Daredevils":
+            temp_array = temp_array + [0, 1, 0, 0, 0, 0, 0, 0]
+        elif batting_team == "Kings XI Punjab":
+            temp_array = temp_array + [0, 0, 1, 0, 0, 0, 0, 0]
+        elif batting_team == "Kolkata Knight Riders":
+            temp_array = temp_array + [0, 0, 0, 1, 0, 0, 0, 0]
+        elif batting_team == "Mumbai Indians":
+            temp_array = temp_array + [0, 0, 0, 0, 1, 0, 0, 0]
+        elif batting_team == "Rajasthan Royals":
+            temp_array = temp_array + [0, 0, 0, 0, 0, 1, 0, 0]
+        elif batting_team == "Royal Challengers Bangalore":
+            temp_array = temp_array + [0, 0, 0, 0, 0, 0, 1, 0]
+        elif batting_team == "Sunrisers Hyderabad":
+            temp_array = temp_array + [0, 0, 0, 0, 0, 0, 0, 1]
+
+        bowling_team = request.form["bowling-team"]
+        if bowling_team == "Chennai Super Kings":
+            temp_array = temp_array + [1, 0, 0, 0, 0, 0, 0, 0]
+        elif bowling_team == "Delhi Daredevils":
+            temp_array = temp_array + [0, 1, 0, 0, 0, 0, 0, 0]
+        elif bowling_team == "Kings XI Punjab":
+            temp_array = temp_array + [0, 0, 1, 0, 0, 0, 0, 0]
+        elif bowling_team == "Kolkata Knight Riders":
+            temp_array = temp_array + [0, 0, 0, 1, 0, 0, 0, 0]
+        elif bowling_team == "Mumbai Indians":
+            temp_array = temp_array + [0, 0, 0, 0, 1, 0, 0, 0]
+        elif bowling_team == "Rajasthan Royals":
+            temp_array = temp_array + [0, 0, 0, 0, 0, 1, 0, 0]
+        elif bowling_team == "Royal Challengers Bangalore":
+            temp_array = temp_array + [0, 0, 0, 0, 0, 0, 1, 0]
+        elif bowling_team == "Sunrisers Hyderabad":
+            temp_array = temp_array + [0, 0, 0, 0, 0, 0, 0, 1]
+
         batsman_type = request.form["Batsman-Type"]
         if batsman_type == "Top":
             temp_array = temp_array + [1, 0, 0]
@@ -144,19 +150,27 @@ def predict():
         elif ball_length == "Yorker":
             temp_array = temp_array + [0, 0, 0, 0, 0, 1]
 
-        temp_array = temp_array + [
-            overs,
-            runs,
-            wickets
-        ]
-
         data = np.array([temp_array])
         my_prediction = int(classifier.predict(data)[0]) # classifier.predict_proba(data)
 
+        # map my_prediction to string value
+
+        if my_prediction == 0:
+            my_prediction = "Extras (Wide, No Ball, Bye, Leg Bye)"
+
+        elif my_prediction == 1:
+            my_prediction = "Dot Ball"
+
+        elif my_prediction == 2:
+            my_prediction = "Runs from the bat!"
+
+        elif my_prediction == 3:
+            my_prediction = "WICKET!"
+
         return render_template(
             "result.html",
+            my_prediction=my_prediction
         )
-
 
 if __name__ == "__main__":
     app.run(debug=True)
