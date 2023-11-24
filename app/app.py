e164e2f6ae834939deee8fb46ed3307900d1f2ce
nakulpadalkar <2807210+nakulpadalkar@users.noreply.github.com>
2023-11-24 14:49:54 -0500

    local docker run

--- app/app.py
+++ app/app.py
@@ -181,6 +181,6 @@ def predict():
         )
 
 if __name__ == "__main__":
-    app.run(debug=T)
+    app.run(debug=True)
     # port = int(os.environ.get("PORT", 5000))
     # app.run(host='0.0.0.0', port=port, debug=True)
